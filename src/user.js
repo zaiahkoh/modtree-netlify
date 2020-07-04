374d40d93dbda5a022305edcbfe35a345d1ef8e3
Isaiah Koh <zaiah0505@hotmail.com>
2020-07-04 11:25:09 +0800

    starting user endpoint

--- src/user.js
+++ src/user.js
@@ -1,21 +1,41 @@
 const express = require("express");
-const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../config/keys");
-const providers = require('../config/providers');
-const https = require('https');
-const validateRegisterInput = require('../utils/register');
-const validateLoginInput = require("../utils/login");
+const mongoose = require('mongoose');
 const User = require('../models/User');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+mongoose.connect(
+  'mongodb+srv://zaiah:modtree@cluster0-scnbi.gcp.mongodb.net/modtree?retryWrites=true&w=majority', 
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+function createJWT (user, callback) {
+  const payload = {
+    id: user.id,
+    name: user.name,
+    email: user.email
+  }
+  jwt.sign(
+    payload,
+    keys.secretOrKey,
+    {
+      expiresIn: 31556926 // 1 year in seconds
+    },
+    (err, token) => {
+      callback(err, token);
+    }
+  );
+}
 
 router.get('/', async (req, res) => {
-  const col = await getCol('rules');
-  const out = await col.findOne({tag: 'r_de_basic'});
-  console.log(out);
-  res.send('hello world');
+  User.findOne({email: "test@test.com"})
+  .then(
+    console.log
+  )
+  res.send('hello user');
 });
 
-app.use('/.netlify/functions/app', router);
+app.use('/.netlify/functions/user', router);
 
 module.exports.handler = serverless(app);
